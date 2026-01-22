a86a7f060ffa13a4a6844397ac25e6963174478d
tjctuhu <tangjiacheng@tuhu.cn>
2026-01-23 00:21:09 +0800

    feat: REMHB2026 v0.5

---
+++ RemHB!/2026/js/bg.js
@@ -0,0 +1,33 @@
+
+const bg = document.getElementsByClassName("bg")[0];
+const aiyiRemUrls = Array.from({ length: 9 }, (_, index) => `../aiyiRemPublic/${index + 1}.webp`);
+
+const createColumn3D = () => {
+    const edge = 36;
+    const row = 3;
+    const standardHeight = 150 / row;
+    const radio = 9 / 16;
+    for (let e = 0; e < edge; e++) {
+        for (let r = 0; r < row; r++) {
+            const aiyiRemAlbum = document.createElement("div");
+            aiyiRemAlbum.className = "aiyiRemAlbum";
+            const scale = Math.random() * 0.5 + 1.2;
+            const rotateDeg = (Math.random() * 2 - 1) * 30;
+            // 居中
+            aiyiRemAlbum.style.top = `${50 - standardHeight / 2 * scale}vh`;
+            aiyiRemAlbum.style.left = `${50 - radio * standardHeight / 2 * scale}vh`;
+            aiyiRemAlbum.style.width = `${radio * standardHeight * scale}vh`;
+            aiyiRemAlbum.style.height = `${standardHeight * scale}vh`;
+            // row/2时translateY为0
+            aiyiRemAlbum.style.transform = `rotateY(${e * 360 / edge}deg) translateZ(1000px) translateY(${(r - row / 2) * standardHeight}vh) rotate(${rotateDeg}deg)`;
+            aiyiRemAlbum.style.backgroundImage = `url(${aiyiRemUrls[Math.floor(Math.random() * aiyiRemUrls.length)]})`;
+            aiyiRemAlbum.style.backgroundSize = "cover";
+            aiyiRemAlbum.style.backgroundPosition = "center";
+            aiyiRemAlbum.style.backgroundRepeat = "no-repeat";
+            aiyiRemAlbum.style.zIndex = `${e*row+r}`;
+            bg.appendChild(aiyiRemAlbum);
+        }
+    }
+}
+
+createColumn3D();
